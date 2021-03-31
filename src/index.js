18824670cfc88a2d250ec3b25bb73244ad8bad45
Odilon <odilonsimpore@gmail.com>
2021-03-31 08:42:43 +0000

    Update index.js
    how to write ES6 string in jsx :)

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ const luckyNum = Math.floor(Math.random() * 23);
 ReactDOM.render(
   <div>
     <h1>
-      Hello everyone, i am {fName}
-      {lName}!
+      Hello everyone, i am {`${fName} ${lName}`}!
     </h1>
     <p>my lucky number is {luckyNum}</p>
   </div>,
