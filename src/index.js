fc16d24922aca4ee877bf1af44d9c08beb825675
Odilon <primarchsys@gmail.com>
2021-03-31 08:30:17 +0000

    Odisys
    let start a basic #jsx revi part in reactjs

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "diba";
+const lName = "od";
+const luckyNum = Math.floor(Math.random() * 23);
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello everyone, i am {fName}
+      {lName}!
+    </h1>
+    <p>my lucky number is {luckyNum}</p>
+  </div>,
+  document.getElementById("root")
+);
